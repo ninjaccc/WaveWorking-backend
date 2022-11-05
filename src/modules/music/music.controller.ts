1a4e71b4401a51c71d2478b0cb3b9d68335717fa
wins <win4709hi@gmail.com>
2022-11-05 20:03:58 +0800

    style: add annotation

--- src/modules/music/music.controller.ts
+++ src/modules/music/music.controller.ts
@@ -3,31 +3,39 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   Query,
-  HttpException,
 } from '@nestjs/common';
 import { MusicService } from './music.service';
 import { AddMusicByUrlOrIdDto } from './dto/add-music-by-url-or-id.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('music')
 @Controller('music')
 export class MusicController {
   constructor(private readonly musicService: MusicService) {}
 
+  /* Search music, and return music list */
+  /*--------------------------------------------*/
   @Get('search')
   search(@Query() query: Record<string, string>) {
     return this.musicService.searchByQuery(query);
   }
 
+  /* Add music, and return this music info */
+  /*--------------------------------------------*/
   @Post()
   async addByUrlOrId(@Body() addMusicByUrlOrIdDto: AddMusicByUrlOrIdDto) {
     return this.musicService.addByUrlOrId(addMusicByUrlOrIdDto);
   }
 
-  // --------------------
-  // use for testing
+  // below is only use for testing
+  // --------------------------------
+  // --------------------------------
+  // --------------------------------
+  // --------------------------------
+  // --------------------------------
   @Get(':id')
   getInfo(@Param('id') id: string) {
     return this.musicService.getInfoById(id);
