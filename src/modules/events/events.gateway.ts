46053c77a5f9a7f491bc59e6ccbc7a9591cd0378
wins <win4709hi@gmail.com>
2022-11-20 20:13:54 +0800

    feat: test for websocket

--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -10,20 +12,38 @@ import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server } from 'ws';
 
-@WebSocketGateway(8080)
+@WebSocketGateway()
 @Injectable()
-export class EventsGateway {
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
+  /** 有人連進來的時候觸發 */
   handleConnection(client: WebSocket, request: IncomingMessage) {
-    console.log(request.url);
+    // #TODO request.socket.remoteAddress可以取得客戶端ip
+    client.send(
+      JSON.stringify({
+        event: 'connect',
+        data: 'success',
+      }),
+    );
   }
 
-  @SubscribeMessage('events')
-  onEvent(client: any, data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'events', data: item })),
-    );
+  handleDisconnect(client: WebSocket) {
+    // #TODO 判斷用戶關閉網站後，如何知道是該userId離開頻道
+    console.log(client.url);
+  }
+
+  // onEvent(client: WebSocket, data: any): Observable<WsResponse<number>> {
+  @SubscribeMessage('test')
+  onEvent(client: any, data: any) {
+    console.log('-------------------');
+    console.log(data);
+    this.server.clients.forEach((single) => {
+      single.send(JSON.stringify(data));
+    });
+    // return from([1, 2, 3]).pipe(
+    //   map((item) => ({ event: 'events', data: item })),
+    // );
   }
 }
