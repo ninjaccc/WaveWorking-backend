e75ff877c70627c4e830539a5d1d38a17228c411
wins <wins@hjtech.tw>
2022-11-21 16:32:06 +0800

    fix: correct env variable name

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { EventsModule } from './modules/events/events.module';
       )}:${encodeURIComponent(
         process.env.MONGO_PASSWORD,
       )}@cluster0.q9hzcmn.mongodb.net/${encodeURIComponent(
-        process.env.MONGO_COLLECTION_NAME,
+        process.env.MONGO_DB_NAME,
       )}?retryWrites=true&w=majority`,
     ),
     UsersModule,
