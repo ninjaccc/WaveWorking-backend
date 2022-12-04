b97977009110188d03cabd6d0302c59ff428556b
wins <win4709hi@gmail.com>
2022-12-05 04:02:13 +0800

    feat: add channelId and userId to music schema

--- src/modules/music/music.schema.ts
+++ src/modules/music/music.schema.ts
@@ -74,6 +74,22 @@ export class Music {
   })
   duration: string;
 
+  // which channel is the music added
+  @ApiProperty({
+    example: '637b38f08e32266d5b5202fa',
+    format: 'string',
+    required: true,
+  })
+  channelId: string;
+
+  // Who added this music
+  @ApiProperty({
+    example: '637c85907f2cb79eb4608ea3',
+    format: 'string',
+    required: true,
+  })
+  userId: string;
+
   // on time
   @Prop()
   @ApiProperty({
