c5d01fcb63d7138c0c90ecefa328e18055f58bb2
wins <win4709hi@gmail.com>
2023-01-27 00:53:32 +0800

    style: add annotation

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // #NOTICE 好像在第二個參數加上0.0.0.0，連線就會一直斷開並且持續重連
   await app.listen(3000);
 }
 bootstrap();
